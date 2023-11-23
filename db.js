9e163a3c293f5dc40deba0cc722703d3cec9f050
Yahia FERCHOULI <yahiaferchouli@MacBook-Air-de-Yahia.local>
2023-11-23 14:54:25 +0100

    add connection to postgres

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize')
-
+const databaseConnectionString = 'postgres://db_render_d2sj_user:c6m4pOPewHacHAXmf3Zu2PpICIGjjHT9@dpg-clflcg6f27hc739dg2d0-a/db_render_d2sj';
 // Database
 const sequelize = new Sequelize(
-  '', // TODO: database connection string
+  databaseConnectionString, // TODO: database connection string
   {
     dialect: 'postgres',
     dialectOptions: {
