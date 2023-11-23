8f688b6ae7ba91d9e0e800f45f6899e31766a849
Yahia FERCHOULI <yahiaferchouli@MacBook-Air-de-Yahia.local>
2023-11-23 14:59:38 +0100

    modify app.js connection to db

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const sequelize = require('./sequelize');
 
 
 const indexRouter = require('./routes/index')
-// const usersRouter = require('./routes/users')
+const usersRouter = require('./routes/users')
 
 const app = express()
 
@@ -29,7 +29,7 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-// app.use('/users', usersRouter)
+app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
